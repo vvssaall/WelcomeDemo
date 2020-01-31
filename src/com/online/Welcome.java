e066bd3995df5982b75c4c8fecae395608ef29e0
Vishal KC <Vishal.kc1159@gmail.com>
2020-01-31 09:59:47 -0500

    first commit

---
+++ src/com/online/Welcome.java
@@ -0,0 +1,23 @@
+package com.online;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class Welcome {
+	@RequestMapping("sayWelcome.htm")
+	/*public void sayWelcome() { 
+		System.out.println("Welcome");
+	*/
+	
+	public ModelAndView sayWelcome() {
+	System.out.println("Welcome");
+	
+	String msg="Welcome to Nepal";
+	return new ModelAndView("/hello.jsp","message",msg);
+		
+	}
+
+}
